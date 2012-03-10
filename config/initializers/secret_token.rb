74e19803f00f3681643ebdd50e88de689b5d1ecf
Josiah <legendaryfox@gmail.com>
2012-03-10 22:05:46 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uread::Application.config.secret_token = '1d518b0c5afa47d83ff87c14fa512c250939ff7e9586503917702b8907aade3035741de07165605fcd822d363f673edc95c99b4983fd8b0bbbcd77a5a4fc8157'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Uread::Application.config.session_store :cookie_store, key: '_uread_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Uread::Application.config.session_store :active_record_store
