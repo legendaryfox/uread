753235c9b73e30e733759546d26b92331e64e45e
Josiah <legendaryfox@gmail.com>
2012-03-11 01:13:47 +0800

    Heroku setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Uread::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  # For heroku
+  config.assets.initialize_on_precompile = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
