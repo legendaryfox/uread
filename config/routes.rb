07b4af14ca5a5ddb28535ad0465a4381fd7ef537
Josiah <legendaryfox@gmail.com>
2012-03-10 23:43:02 +0800

    Changes to database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Uread::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Uread::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
